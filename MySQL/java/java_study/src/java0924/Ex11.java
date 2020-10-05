6f6b3b3167df99892238963bad6978d30dedeeb3
otx-koll <ihs1836@gmail.com>
2020-10-05 10:00:52 +0900

    a

--- MySQL/java/java_study/src/java0924/Ex11.java
+++ MySQL/java/java_study/src/java0924/Ex11.java
@@ -1,3 +1,4 @@
+package java0924;
 
 public class Ex11 {
 	
@@ -8,14 +9,14 @@ public class Ex11 {
 	public static void methodB(int... num) { // 가변인자 메소드
 		// 가변인자 메소드의 매개변수는 배열이 됨
 		System.out.println(num.length);
-		for(int n : num) {
+		for (int n : num) {
 			System.out.print(n + " ");
 		}
 		System.out.println("\n");
 	}
 
 	public static void main(String[] args) {
-		methodA(10, 20, 30); // methodA()는 같은 클래스 안에 있기 때문에 앞에 Ex11.을 붙히지 않아도 됨
+		methodA(10, 20, 30);
 		
 		methodB();
 		methodB(100);
