616f69bae8f4b48cf11961cf4c11acee90e5613a
otx-koll <ihs1836@gmail.com>
2020-10-06 15:02:21 +0900

    a

--- MySQL/java/booklist/src/BookList1.java
+++ MySQL/java/booklist/src/BookList1.java
@@ -1,29 +1,33 @@
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class booklist {
+public class BookList1 {
+	// 1. 멤버변수
 	Connection con;
+	Statement stmt;
+	ResultSet rs;
 	
-	public booklist() {
+	// 2. 생성자
+	public BookList1() {
 		String url = "jdbc:oracle:thin:@localhost:1521:XE";
-		/* 11g express edtion은 orcl 대신 XE를 입력 */
-		String userid = "madang";
-		String pwd = "madang";
-		
-		try { /* 드라이버를 찾는 과정 */
+		String userid = "madang"; // 
+		String pwd = "madang"; // 
+
+		// 1. 드라이버 로드를 위한 (파일 안에 클래스 임포트)
+		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			System.out.println("드라이버 로드 성공");
-		} catch(ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		try { /* 데이터베이스를 연결하는 과정  */
-			System.out.println("데이터베이스 연결 준비 ...");
+		// 2. DB 연결(오라클)
+		try {
+			System.out.println("데이터베이스 연결 준비 .....");
 			con = DriverManager.getConnection(url, userid, pwd);
 			System.out.println("데이터베이스 연결 성공");
 		} catch (SQLException e) {
@@ -31,27 +35,30 @@ public class booklist {
 		}
 	}
 	
-	private void sqlRun() {
-		String query = "SELECT * FROM Book";
+	// 3. 메소드
+	public void sqlRun() { // 생성자
+		String query = "SELECT * FROM book";
 		try {
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
-			System.out.println("BOOK NO \tBOOK NAME \t\t{IBLISHER \tPRICE");
+			stmt = con.createStatement(); // 2
+			rs = stmt.executeQuery(query); // 3
+			System.out.println("BOOK ID \tBOOK NAME \t\tPUBLISHER \tPRICE");
 			while (rs.next()) {
 				System.out.print("\t" + rs.getInt(1));
 				System.out.print("\t" + rs.getString(2));
 				System.out.print("\t\t" + rs.getString(3));
 				System.out.println("\t" + rs.getInt(4));
+
 			}
-			
 			con.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
 	}
-	
-	public static void main(String[] args) {
-		booklist so = new booklist();
-		so.sqlRun();
-	}
+
+//	public static void main(String args[]) { // 메소드이면서 프로그램실행에 관여함
+//		BookList so = new BookList();
+//		so.sqlRun();
+//	}
 }
+
---
+++ MySQL/java/booklist/src/CBasic.java
@@ -0,0 +1,6 @@
+
+public class CBasic {
+	public static void main(String args[]) { // 메소드이면서 프로그램실행에 관여함
+		new BookList1().sqlRun();
+	}
+}
