3b35f1e16dc23fbf66c6f52337cbb2c07fc9ca7a
otx-koll <ihs1836@gmail.com>
2020-10-27 11:43:32 +0900

    a

--- android/ex06_19/app/src/main/java/com/cookandroid/ex06_19/MainActivity.java
+++ android/ex06_19/app/src/main/java/com/cookandroid/ex06_19/MainActivity.java
@@ -2,7 +2,17 @@ package com.cookandroid.ex06_19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 // 1.
 // 액션바(Acrtion Bar)
@@ -21,11 +31,82 @@ import android.os.Bundle;
 // tabSong.setTabListener(this); // 탭을 터치하면 작동하는 리스너 지정
 // bar.addTab(tabSong); // 탭을 액션바에 추가
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements ActionBar.TabListener {
+    ActionBar.Tab tabSong, tabArtist, tabAlbum;
+
+    MyTabFragment myFrags[] = new MyTabFragment[3];
+
+    public void onTabSelected(TabLayout.Tab tab, FragmentTransaction ft) {
+        MyTabFragment myTabFrag = null;
+
+        if (myFrags[tab.getPosition()] == null) {
+            myTabFrag = new MyTabFragment();
+            Bundle data = new Bundle();
+            data.putString("tabName", tab.getText().toString());
+            myTabFrag.setArguments(data);
+            myFrags[tab.getPosition()] = myTabFrag;
+        }
+        else
+            myTabFrag = myFrags[tab.getPosition()];
+
+        ft.replace(android.R.id.content, myTabFrag);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActionBar bar = getActionBar(); // 상단에 표시할 액션바 준비
+        // 탭호스트와 같이 탭의 모양을 가지도록 설정
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        tabSong = bar.newTab(); // 탭을 액션바에 생성
+        tabSong.setText("음악별"); // 탭의 글자 설정
+        tabSong.setTabListener(this); // 탭을 터치하면 작동하는 리스너 지정
+        bar.addTab(tabSong); // 탭을 액션바에 추가
+
+        tabArtist = bar.newTab();
+        tabArtist.setText("가수별");
+        tabArtist.setTabListener(this);
+        bar.addTab(tabArtist);
+
+        tabAlbum = bar.newTab();
+        tabAlbum.setText("앨범별");
+        tabAlbum.setTabListener(this);
+        bar.addTab(tabAlbum);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+
+    public static class MyTabFragment extends androidx.fragment.app.Fragment {
+        String tabName;
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            Bundle data = getArguments();
+            tabName = data.getString("tabName");
+        }
+        public View onCreateView(LayoutInflater inflater, ViewGroup contatiner, Bundle savedInstanceState) {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT);
+            LinearLayout baseLayout = new LinearLayout(super.getActivity());
+            baseLayout.setOrientation(LinearLayout.VERTICAL);
+            baseLayout.setLayoutParams(params);
+
+            if(tabName == "음악별") baseLayout.setBackgroundColor(Color.RED);
+            if(tabName == "가수별") baseLayout.setBackgroundColor(Color.GREEN);
+            if(tabName == "앨범별") baseLayout.setBackgroundColor(Color.BLUE);
+
+            return baseLayout;
+        }
     }
 }
